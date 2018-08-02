f0dc75da09f147d38d20785c6cd10324b060aa94
boobalanv <boobalanv1990@gmail.com>
2018-08-03 00:59:51 +0530

    Added metal credits

--- SpaceMetalSeller/IntergalacticNotation.cs
+++ SpaceMetalSeller/IntergalacticNotation.cs
@@ -25,12 +25,34 @@ namespace SpaceMetalSeller
         {
             string[] metalStmt = metalString.Split(new string[] { "is" }, StringSplitOptions.None);
 
-            string[] unitsMetalStr = metalStmt.FirstOrDefault().Split(' ')
+            string[] unitsMetalStr = metalStmt.FirstOrDefault().Split(' ');
+
+            int totalMetalValue = Convert.ToInt32(metalStmt.LastOrDefault().Split(' ')[0]);
+
+            string romanNumeral = "";
+            string metalName = "";
 
             for (int i = 0; i < unitsMetalStr.Length; i++)
             {
-
+                if(UnitCredits.Keys.Contains(unitsMetalStr[i]))
+                {
+                    romanNumeral = romanNumeral + UnitCredits[unitsMetalStr[i]];
+                }
+                else
+                {
+                    metalName = unitsMetalStr[i];
+                }
             }
+
+            int base10Number = RomanNumeralConvention.ConvertRomanNumeralToBase10(romanNumeral);
+
+            float metalCredit = (float)totalMetalValue / base10Number;
+
+            if(!MetalCredits.Keys.Contains(metalName))
+            {
+                MetalCredits.Add(metalName, metalCredit);
+            }
+
         }
     }
 }
