5e037030d5266d492bf7ab6160de71041d9613d6
boobalanv <boobalanv1990@gmail.com>
2018-08-04 00:10:54 +0530

    finish

--- SpaceMetalSeller.UnitTests/RomanNumeralConventionTest.cs
+++ SpaceMetalSeller.UnitTests/RomanNumeralConventionTest.cs
@@ -14,7 +14,27 @@ namespace SpaceMetalSeller.UnitTests
 
             int base10Number = RomanNumeralConvention.ConvertRomanNumeralToBase10(romanNumber);
 
-            Assert.AreEqual(4, base10Number);
+            Assert.AreEqual(40, base10Number);
+        }
+
+        [TestMethod]
+        public void TestShouldReturn0ForInvalidNumeral()
+        {
+            string romanNumber = "VLD";
+
+            int base10Number = RomanNumeralConvention.ConvertRomanNumeralToBase10(romanNumber);
+
+            Assert.AreEqual(0, base10Number);
+        }
+
+        [TestMethod]
+        public void TestShouldReturn0WhenRepeated4Times()
+        {
+            string romanNumber = "XXXX";
+
+            int base10Number = RomanNumeralConvention.ConvertRomanNumeralToBase10(romanNumber);
+
+            Assert.AreEqual(0, base10Number);
         }
     }
 }
